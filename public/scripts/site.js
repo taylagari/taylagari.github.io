c303a10b444d63d20f6179131ac0f3942ae5276e
Tahmim Hasan Sany <sanytahmim@gmail.com>
2025-10-20 13:20:17 +0600

    logo update

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -2,17 +2,17 @@
 
 
 // Live Chat
-var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-      (function () {
-        var s1 = document.createElement("script"),
-          s0 = document.getElementsByTagName("script")[0];
-        s1.async = true;
-        s1.src = "https://embed.tawk.to/68eb9575ca008419546706c6/1j7c3vlpd";
-        s1.charset = "UTF-8";
-        s1.setAttribute("crossorigin", "*");
-        s0.parentNode.insertBefore(s1, s0);
-      })();
+// var Tawk_API = Tawk_API || {},
+//         Tawk_LoadStart = new Date();
+//       (function () {
+//         var s1 = document.createElement("script"),
+//           s0 = document.getElementsByTagName("script")[0];
+//         s1.async = true;
+//         s1.src = "https://embed.tawk.to/68eb9575ca008419546706c6/1j7c3vlpd";
+//         s1.charset = "UTF-8";
+//         s1.setAttribute("crossorigin", "*");
+//         s0.parentNode.insertBefore(s1, s0);
+//       })();
 
 // Mobile menu toggle
 export function initMobileMenu(toggleId = 'menu-toggle', menuId = 'mobile-menu') {
